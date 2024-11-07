ad699868dcb72fe45e8b799b64a16bdcfa603187
VN\Vincent.lai <Vincentlai0211@gmail.com>
2024-11-07 16:28:23 +0700

    add CartController, add CartView

---
+++ VinWeb/Controllers/ShoppingCartController.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Vin.Web.Models.CartModels;
+using Vin.Web.Service.IService;
+
+namespace Vin.Web.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(LoadcartDTOBasedOnLoggedInUser);
+        }
+
+        private async Task<CartDTO> LoadcartDTOBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDTO response = await _shoppingCartService.GetCartByUserIdAsync(userId);
+            if (response == null && response.IsSuccess)
+            {
+                CartDTO cartDTO = JsonConvert.DeserializeObject<CartDTO>(Convert.ToString(response));
+                return cartDTO;
+            }
+            return new CartDTO();
+        }
+
+
+    }
+}
